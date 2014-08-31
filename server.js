16159f115f1b1941fe562f673679ed2c3dc669d4
Patrick Smith <psmith@psmith-mba.attlocal.net>
2014-08-30 23:31:56 -0700

    cleanup

--- server.js
+++ server.js
@@ -2,7 +2,17 @@
 var connect = require('connect')
     , express = require('express')
     , io = require('socket.io')
-    , port = (process.env.PORT || 8081);
+    , port = (process.env.PORT || 8081)
+    , utility = require('./static/js/utility');
+
+var log = utility.log;
+var logObject = utility.logObject;
+
+var NotFound = function(msg){
+    this.name = 'NotFound';
+    Error.call(this, msg);
+    Error.captureStackTrace(this, arguments.callee);
+}
 
 //Setup Express
 var server = express.createServer();
@@ -11,7 +21,7 @@ server.configure(function(){
     server.set('view options', { layout: false });
     server.use(connect.bodyParser());
     server.use(express.cookieParser());
-    server.use(express.session({ secret: "shhhhhhhhh!"}));
+    server.use(express.session({ secret: "j9wf349fioajf9q4jfajefw98wefj"}));
     server.use(connect.static(__dirname + '/static'));
     server.use(server.router);
 });
@@ -35,55 +45,183 @@ server.error(function(err, req, res, next){
                 },status: 500 });
     }
 });
-server.listen( port);
+server.listen(port);
+var routes = require('./routes')(server);
 
 //Setup Socket.IO
 var io = io.listen(server);
+io.set('close timeout', 60);
+io.set('heartbeat timeout', 15);
+var games = {};
+var userList = {};
+var userCount = 0;
+var uniqueKeyIndex = Math.pow(10, 8);
+var GameState = {
+   WAITING_FOR_PLAYERS: 1
+  ,STARTED: 2
+  ,FIINISHED: 3
+};
+//assign to globals
+global.games = games;
+global.userList = userList;
+global.userCount = userCount;
+global.uniqueKeyIndex = uniqueKeyIndex;
+global.GameState = GameState;
+
+
+//sockets
 io.sockets.on('connection', function(socket){
-  console.log('Client Connected');
-  socket.on('message', function(data){
-    socket.broadcast.emit('server_message',data);
-    socket.emit('server_message',data);
+
+  log('Client Connected');
+
+  var loginUser = function(data) {
+
+    var userSession = data.user;
+    var game = data.game;
+    //add to global user list
+    userList[socket.id] = {
+       username: userSession
+      ,id: socket.id
+      ,game: game
+    }
+    socket.nickname = userSession;
+    userCount++;
+    log('loginUser userList: ' + logObject(userList));
+    if (typeof games[game] === 'undefined') {
+      games[game] = {
+         userList: {}
+        ,userCount: 0
+        ,state: GameState.WAITING_FOR_PLAYERS
+      }
+      log('loginUser created new game: ' + game);
+    }
+
+    if (typeof games[game].userList[userSession] !== 'undefined' && games[game].userList[userSession].id !== socket.id ) {
+      log('duplicate username');
+      log(logObject(games[game].userList));
+      //collision - let's keep names unique so we can easily target specific users
+      socket.emit('loginError', { 
+        error: 'Please choose a unique name.'
+      });
+    } else {
+      log('unique user, join game');
+      if (games[game].state === GameState.WAITING_FOR_PLAYERS) {
+        //add to game's user list
+        games[game].userList[userSession] = userList[socket.id];
+        games[game].userList[userSession].ready = false;
+        games[game].userCount++;
+        log('loginUser game: ' + logObject(games[game]));
+        log('login request from ' + socket.nickname);
+
+        //join game - new room
+        socket.join(game);
+
+        io.sockets.in(game).emit('updatebuddies', {
+          users: games[game].userList,
+          newUser: userSession
+        });
+        socket.emit('loginSuccess', {}); 
+        socket.emit('updatebuddies', {
+          users: games[game].userList,
+          newUser: userSession
+        });
+        log('Users:' + userCount);
+      } else {
+        //send error to socket
+        socket.emit('loginError', { 
+          error: 'Sorry, this game has already started. Join a new lobby.'
+        });
+      }
+    }
+
+  };
+
+  socket.on('login', loginUser);
+    
+  socket.on('doAction', function(data) {
+    log('doing action');
   });
+  
+  socket.on('ready', function(data) {
+    if (typeof games[data.game] !== 'undefined') {
+      var startGame = true;
+      var game = games[data.game];
+      log('ready: ' + logObject(game));
+      if (typeof game.userList[data.user] !== 'undefined') {
+        game.userList[data.user].ready = true;
+        for (var username in game.userList) {
+          var user = game.userList[username];
+          if (typeof user !== 'undefined') {
+            startGame &= user.ready;
+          }
+          if (!startGame) {
+            break;
+          }
+        }
+      } else {
+        startGame = false;
+      }
+      
+      if (startGame) {
+        game.state = GameState.STARTED;
+        io.sockets.in(data.game).emit('startGame', {
+        }); 
+      } else {
+        game.state = GameState.WAITING_FOR_PLAYERS;
+        io.sockets.in(data.game).emit('ready', {
+          readyUser: data.user
+        }); 
+      }
+      
+    }
+  });
+
   socket.on('disconnect', function(){
-    console.log('Client Disconnected.');
-  });
-});
+    log('Client Disconnected.');
+    var leftUser = socket.nickname;
+    if (typeof userList[socket.id] !== 'undefined') {
+      var usersGame = userList[socket.id].game;
+      if (typeof games[usersGame] !== 'undefined') {
+        log('disconnect: ' + logObject(games[usersGame]));
+        log(socket.nickname + ' disconnected');
+        games[usersGame].userList[socket.nickname] = undefined;
 
+        socket.leave(usersGame);
 
-///////////////////////////////////////////
-//              Routes                   //
-///////////////////////////////////////////
-
-/////// ADD ALL YOUR ROUTES HERE  /////////
-
-server.get('/', function(req,res){
-  res.render('index.jade', {
-    locals : { 
-              title : 'Your Page Title'
-             ,description: 'Your Page Description'
-             ,author: 'Your Name'
-             ,analyticssiteid: 'XXXXXXX' 
+        if (games[usersGame].userCount > 0) {
+          games[usersGame].userCount--;
+        }
+        if (games[usersGame].userCount <= 0) {
+          games[usersGame] = undefined;
+          delete games[usersGame];
+        } else {
+          var startGame = true;
+          for (var user in games[usersGame].userList) {
+            var user = games[usersGame].userList[user];
+            if (typeof user !== 'undefined' && !user.ready) {
+              startGame = false;
+              break;
             }
+          }
+          if (startGame) {
+            games[usersGame].state = GameState.STARTED;
+            io.sockets.in(usersGame).emit('startGame');
+          }
+          io.sockets.in(usersGame).emit('logout', {
+            leftUser: leftUser,
+            users: games[usersGame].userList
+          });
+          log(leftUser + ' left game ' + usersGame)
+        }
+      }
+      userList[socket.id] = undefined;
+      if (userCount > 0) {
+        userCount--;
+      }
+    } else {
+      log('orphan session disconnceted');
+    }
   });
 });
 
-
-//A Route for Creating a 500 Error (Useful to keep around)
-server.get('/500', function(req, res){
-    throw new Error('This is a 500 Error');
-});
-
-//The 404 Route (ALWAYS Keep this as the last route)
-server.get('/*', function(req, res){
-    throw new NotFound;
-});
-
-function NotFound(msg){
-    this.name = 'NotFound';
-    Error.call(this, msg);
-    Error.captureStackTrace(this, arguments.callee);
-}
-
-
-console.log('Listening on http://0.0.0.0:' + port );
+log('Listening on http://127.0.0.1:' + port );
--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,203 @@
-/* Author: YOUR NAME HERE
-*/
+var timeout = null;
 
-$(document).ready(function() {   
+var updateUserList = function(users) {
+  $('#buddylist').empty();
+  for (var userId in users) {
+    var userClass = '';
+    if (userId === username) {
+      userClass = 'class="user-self" title="This is you"';
+    }
+    $('#buddylist').append('<li id="' + userId + '" ' + userClass + '">' + 
+      users[userId].username + '</li>');
+    $('#' + userId + ':not(.user-self)').on('click', function() {
+      $('#msg').val($('#msg').val() + ' ' + users[userId].username + ' ');
+      $('#msg').focus();
+    });
+  }
+  if (timeout !== null) clearTimeout(timeout);
+  $('#buddylist').slideDown({queue: false});
+  $('.floating-info').animate({opacity: 1, queue:false});
+  $('#buddylist').animate({opacity: 1, queue:false});
+  timeout = setTimeout(function() {
+    $('#buddylist').slideUp({queue: false});
+    $('.floating-info').animate({opacity: 0.3, queue:false});
+    $('#buddylist').animate({opacity: 0.3, queue:false});
+  }, 5000);
+};
 
+$(document).ready(function() {  
+  document.ontouchmove = function(event) {
+    if (event.target != $('#msg') && event.target != $('#send')) {
+      event.preventDefault();
+    }
+  };
+
+  scrollBottom($('#log'));
+  
+  //setup socket connection
   var socket = io.connect();
+  
+  //perform a command using msgHandlers or send a plain message
+  var sendMessage = function() {
+    var color = Color.random();
+    console.log(color);
+    var hasError = false;
+    var msg = $('#msg').val();
+    var target = null;
+    var isCommand = false;
+    if (msg.indexOf('/') > -1) {
+      //command msg
+      var cmdMsg = msg.trim();
+      for (var command in msgHandlers) {
+        var index = cmdMsg.indexOf('/' + command);
+        if (index == 0 && typeof msgHandlers[command] !== 'undefined') {
+          var params = cmdMsg.substring(index + command.length + 1).match(/\S+/g);
+          var result = msgHandlers[command](params, msg, hasError);
+          hasError = result.error;
+          msg = result.message;
+          target = result.target;
+	  isCommand = true;
+          break;
+        }
+      }
+    }
+    socket.emit('sendMsg', {
+      color: color, 
+      message: msg, 
+      user: username,
+      command: isCommand,
+      local: hasError,
+      to: target,
+      from: username
+    });  
+    $('#msg').val('');
+  }.bind(socket);
 
-  $('#sender').bind('click', function() {
-   socket.emit('message', 'Message Sent on ' + new Date());     
+  //login
+  $('#log').val('');
+  socket.emit('login', {user: username});
+
+  //choose new name if current one is taken
+  socket.on('loginerror', function(data) {
+    if (data.error) {
+      $('#error-msg').html(data.error);
+    }
+    $('#chat-content').hide();
+    $('#login-error').fadeIn('fast');
+  });
+  
+  //submit on click
+  $('#send').on('click', function() {
+    sendMessage();  
+  });
+  $('#retry-login').on('click', function() {
+    username = $('#new-username').val();
+    $('#login-error').fadeOut('fast');
+    socket.emit('login', {user: username});
+  });
+  
+  //submit on enter
+  $('#msg').keyup(function(event) {
+    if ( event.which == 13 ) {
+      sendMessage();
+    }
+  });
+  $('#new-username').keyup(function(event) {
+    if ( event.which == 13 ) {
+      username = $('#new-username').val();
+      $('#login-error').fadeOut('fast');
+      socket.emit('login', {user: username});
+    }
   });
 
-  socket.on('server_message', function(data){
-   $('#receiver').append('<li>' + data + '</li>');  
+  //silly events - make random colors animate on everyone's screen
+  var interval;
+  socket.on('dance', function() {
+    interval = setInterval(function() {
+      $('#colorspace').css('background-color', Color.random());
+    }.bind(interval), 100);
   });
+  
+  socket.on('stop', function() {
+    window.clearInterval(interval);
+  }.bind(interval));
+
+  //clear chat window for everyone
+  socket.on('clear', function() {
+    window.clearInterval(interval);
+    $('#log').html('');
+  }.bind(interval));
+  
+  //update color and display message received
+  socket.on('applyColor', function(data){
+    if (data.color) {
+      $('#colorspace').css("background-color", data.color);
+    }
+    if (data.message.length) {
+      var commandMsg = '';
+      var msgClass = '';
+      if (data.to && data.to !== data.from) {
+        if (data.from == username) {
+          commandMsg = 'Message to ' + data.to + ': ';
+          msgClass = 'message-to';
+        } else if (data.to == username) {
+          commandMsg = 'Message from ' + data.from + ': ';
+          msgClass = 'message-from';
+        }
+      }
+      appendMessage(((!data.command) ? data.user + ': ' : '\u25CF ' + commandMsg) + 
+        data.message, msgClass);	
+    }
+
+    $('#msg').focus();
+
+  });
+  
+
+  //update user list when people join or leave chat
+  socket.on('updatebuddies', function(data) {
+    if (!$('#chat-content').is(':visible')) {
+      $('#chat-content').fadeIn('fast');
+      if (username === data.newUser) {
+        //this should be more flexible
+        var urlParts = window.location.href.split('/');
+        //first two parts are http: and an empty string
+        //second part is domain
+        //third part is color string
+
+        urlParts[4] = data.newUser;
+        window.history.pushState({}, document.title, urlParts.join('/')); 
+      }
+    }
+    appendMessage(data.newUser + ' has joined the chat.');
+    updateUserList(data.users);
+  });
+
+  socket.on('logout', function(data) {
+    appendMessage(((data.leftUser) ? data.leftUser.username : 'Someone') + ' has left the chat.');
+    updateUserList(data.users);
+    
+  });
+  
+
+  //show and hide user list
+  $('.floating-info').on('mouseenter', function() { 
+    if (timeout !== null) clearTimeout(timeout);
+    if (!$('#buddylist').is(':visible')) {
+      $('#buddylist').slideDown({queue: false});
+    }
+    $('.floating-info').animate({opacity: 1, queue: false});
+    $('#buddylist').animate({opacity: 1, queue: false});
+  });
+  $('.floating-info').on('mouseleave',function() {
+    timeout = setTimeout(function() {
+      if ($('#buddylist').is(':visible')) {
+        $('#buddylist').stop().slideUp({queue: false})
+          .animate({opacity: 0.3, queue: false});
+      }
+      $('.floating-info').stop().animate({opacity: 0.3, queue: false});
+    }, 500);
+  });
+
+  $('#msg').focus();
 });
