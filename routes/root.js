db58b8e4cd03e0a2e1850761e6d15459aaa869ed
Patrick Smith <psmith@psmith-mba.attlocal.net>
2014-08-31 14:09:33 -0700

    add library function

--- routes/root.js
+++ routes/root.js
@@ -1,5 +1,16 @@
+var resources = require('../static/js/games/resources');
 module.exports = function(server) {
-
+	server.get('/', function(req, res){
+        res.render('library.jade', {
+	 		locals: {
+	 			games: Object.keys(resources.gameResources)
+	           ,title : 'Board Game Library'
+	           ,description: 'Board Game Library'
+	           ,author: 'Patrick Smith'
+	           ,analyticssiteid: 'XXXXXXX' 
+	          }
+	       });
+    });
     //A Route for Creating a 500 Error (Useful to keep around)
     server.get('/500', function(req, res){
         throw new Error('This is a 500 Error');
